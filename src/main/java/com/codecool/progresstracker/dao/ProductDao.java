99d4e36eff0d8ed2aadbc51a18332ffaeceea08b
Csilla Lukacs <erzsebet.lk@gmail.com>
2021-11-30 19:10:53 +0100

    feat: add @Service annotation to ProductDao so Spring Boot can recognise it

--- src/main/java/com/codecool/progresstracker/dao/ProductDao.java
+++ src/main/java/com/codecool/progresstracker/dao/ProductDao.java
@@ -2,10 +2,12 @@ package com.codecool.progresstracker.dao;
 
 import com.codecool.progresstracker.model.Product;
 import com.codecool.progresstracker.model.User;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public interface ProductDao {
 
     void add(Product product);
