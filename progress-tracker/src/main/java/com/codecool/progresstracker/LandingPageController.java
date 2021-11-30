fafe74f47cc03133fc6218ab262437302d1de496
Papp Bence <papp.bence15@gmail.com>
2021-11-30 11:53:04 +0100

    feat: Create user class

--- progress-tracker/src/main/java/com/codecool/progresstracker/LandingPageController.java
+++ progress-tracker/src/main/java/com/codecool/progresstracker/LandingPageController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LandingPageController {
 
     @GetMapping("/")
-    public String welcomePage(){
+    public String welcomePage() {
         return "welcome";
     }
-
-
 }
