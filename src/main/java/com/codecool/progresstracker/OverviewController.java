4f7c150d206aff88b1c5c8497fe3bdaa5eb1a594
Csilla Lukacs <erzsebet.lk@gmail.com>
2021-11-30 17:59:42 +0100

    feat: Add userDao attribute to OverviewController; productPage gets user from UserDao, passes it on to the template

--- src/main/java/com/codecool/progresstracker/OverviewController.java
+++ src/main/java/com/codecool/progresstracker/OverviewController.java
@@ -1,17 +1,28 @@
 package com.codecool.progresstracker;
 
+import com.codecool.progresstracker.dao.UserDao;
 import com.codecool.progresstracker.model.User;
 import com.codecool.progresstracker.model.UserType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.HttpClientErrorException;
 
 @Controller
 public class OverviewController {
+    UserDao userDao;
+
+    @Autowired
+    public OverviewController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @GetMapping("/newsfeed")
-    public String productPage(@RequestBody User user){
+    public String productPage(Model model){
+        User user = userDao.getFirstUser();
+        model.addAttribute("user", user);
         UserType userType = user.getUserType();
         if(userType.equals(UserType.PRODUCT_OWNER)){
             return "index";
