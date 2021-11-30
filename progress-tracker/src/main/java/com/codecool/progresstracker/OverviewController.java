a69f046924ec376f7adeece4a8cdd1596e988022
Papp Bence <papp.bence15@gmail.com>
2021-11-30 13:44:46 +0100

    feat: Return the correct html

--- progress-tracker/src/main/java/com/codecool/progresstracker/OverviewController.java
+++ progress-tracker/src/main/java/com/codecool/progresstracker/OverviewController.java
@@ -1,14 +1,24 @@
 package com.codecool.progresstracker;
 
 import com.codecool.progresstracker.model.User;
+import com.codecool.progresstracker.model.UserType;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Controller
 public class OverviewController {
     @GetMapping("/newsfeed")
     public String productPage(@RequestBody User user){
-        return null;
+        UserType userType = user.getUserType();
+        if(userType.equals(UserType.PRODUCT_OWNER)){
+            return "index";
+        }else if(userType.equals(UserType.ADMIN)){
+            return "admin-index";
+        }else{
+            throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
