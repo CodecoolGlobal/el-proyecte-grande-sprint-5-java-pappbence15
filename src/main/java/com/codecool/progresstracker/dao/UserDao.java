a6a4b103a95c1843f7bb5e92f1f958385e204b33
Csilla Lukacs <erzsebet.lk@gmail.com>
2021-11-30 17:49:13 +0100

    feat: initial UserDao interface, more methods can be added later as needed

---
+++ src/main/java/com/codecool/progresstracker/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.codecool.progresstracker.dao;
+
+import com.codecool.progresstracker.model.User;
+
+import java.util.UUID;
+
+public interface UserDao {
+    User find(UUID id);
+    void add(User user);
+    User getFirstUser(); //for testing, until we can get logged in user from session or whatever
+}
