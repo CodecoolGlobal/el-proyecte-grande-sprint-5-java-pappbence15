a7871f309e6d1c3f246110365980b5c03066158a
Papp Bence <papp.bence15@gmail.com>
2021-11-30 11:00:12 +0100

    feat: Index page's endpoint

---
+++ progress-tracker/src/main/java/com/codecool/progresstracker/LandingPageController.java
@@ -0,0 +1,13 @@
+package com.codecool.progresstracker;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LandingPageController {
+
+    @GetMapping("/")
+    public String indexPage(){
+        return "index";
+    }
+}
