efcb577ca1607711f19207b0577977c6b2e0ff0e
Papp Bence <papp.bence15@gmail.com>
2021-11-30 11:20:34 +0100

    update: Change index to welcome according to the plan

--- progress-tracker/src/main/java/com/codecool/progresstracker/LandingPageController.java
+++ progress-tracker/src/main/java/com/codecool/progresstracker/LandingPageController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LandingPageController {
 
     @GetMapping("/")
-    public String indexPage(){
-        return "index";
+    public String welcomePage(){
+        return "welcome";
     }
+
+
 }
