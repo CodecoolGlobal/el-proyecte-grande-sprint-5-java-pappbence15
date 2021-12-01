443611082f75ad8a64ebf34b50ff71760a8b802e
Papp Bence <papp.bence15@gmail.com>
2021-12-01 11:46:36 +0100

    feat: Create endpoint for updating user story

--- src/main/java/com/codecool/progresstracker/controllers/view/DetailedViewController.java
+++ src/main/java/com/codecool/progresstracker/controllers/view/DetailedViewController.java
@@ -1,13 +1,33 @@
 package com.codecool.progresstracker.controllers.view;
 
 import com.codecool.progresstracker.model.User;
+import com.codecool.progresstracker.model.UserStory;
+import com.codecool.progresstracker.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+@Controller
 public class DetailedViewController {
+
+    private final ProductService productService;
+
+    @Autowired
+    DetailedViewController(ProductService productService){
+        this.productService = productService;
+    }
 
     @GetMapping("/detailedProductFeed")
     public String productPage(@RequestBody User user){
+        return null;
+    }
+
+    @PostMapping("/detailedProductFeed/{product}/{userStory}")
+    public String changeDetails(@PathVariable String product,
+                                @PathVariable String userStory,
+                                @RequestBody UserStory changedUserStory){
         return null;
     }
 }
