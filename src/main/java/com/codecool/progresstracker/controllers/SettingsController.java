8d76fd0f57ca9280b0ea585da00cf9f0bb1daac2
Papp Bence <papp.bence15@gmail.com>
2021-12-01 11:27:02 +0100

    feat: Settings endpoints

--- src/main/java/com/codecool/progresstracker/controllers/SettingsController.java
+++ src/main/java/com/codecool/progresstracker/controllers/SettingsController.java
@@ -1,8 +1,22 @@
 package com.codecool.progresstracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SettingsController {
 
+    @GetMapping("/{userName}/settings")
+    public String getSettings(@PathVariable String userName){
+        //todo: return user's settings
+        return null;
+    }
+
+    @PostMapping("/{userName}/settings")
+    public String setSettings(@PathVariable String userName){
+        //todo: set user's settings accordingly
+        return null;
+    }
 }
