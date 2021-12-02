35e1ed03a47d3791f6106ef7e9a6743e6e5119f6
Csilla Lukacs <erzsebet.lk@gmail.com>
2021-12-02 10:18:17 +0100

    feat: Redirect to admin or owner projects view based on logged in user

--- src/main/java/com/codecool/progresstracker/controllers/LandingPageController.java
+++ src/main/java/com/codecool/progresstracker/controllers/LandingPageController.java
@@ -1,13 +1,30 @@
 package com.codecool.progresstracker.controllers;
 
+import com.codecool.progresstracker.model.User;
+import com.codecool.progresstracker.model.UserType;
+import com.codecool.progresstracker.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LandingPageController {
+    private final UserService userService;
+
+    public LandingPageController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public String welcomePage() {
+
+        User user = userService.getLoggedInUser();
+
+        if (user.getUserType().equals(UserType.ADMIN)){
+            return "redirect:/admin/projects";
+        } else if (user.getUserType().equals(UserType.PRODUCT_OWNER)){
+            return "redirect:/admin/projects";
+        }
+
         return "welcome";
     }
 }
