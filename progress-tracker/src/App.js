151dd88a831a772883dc5da854aa37544350c9ec
Papp Bence <papp.bence15@gmail.com>
2021-12-14 14:17:08 +0100

    feat: Setting component state

--- progress-tracker/src/App.js
+++ progress-tracker/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from "./Navbar";
+import {useState} from "react";
 
 function App() {
+    const [component, setComponent] = useState('All')
+    const changeComponent = (newComponent) => setComponent(newComponent)
+    console.log(component)
   return (
     <div className="App">
       <header className="App-header">
       </header>
-        <Navbar/>
+        <Navbar change={changeComponent}/>
     </div>
   );
 }
