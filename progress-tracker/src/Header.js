2e31079399de4bd0c79143fb5c2eed2cbfd0db9f
Papp Bence <papp.bence15@gmail.com>
2021-12-14 12:01:13 +0100

    feat: Links for header's navbar

---
+++ progress-tracker/src/Header.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+function Header(){
+    const projects = 'Projects';
+    const latest = 'Latest';
+    const all = 'All';
+
+    return(
+        <div>
+            <div>
+                {logo}
+            </div>
+            <div>
+                {renderNavLink('#', projects)}
+            </div>
+            <div>
+                {renderNavLink('/', latest)}
+            </div>
+            <div>
+                {renderNavLink('/', all)}
+            </div>
+        </div>
+    )
+}
+
+function renderNavLink(url, name){
+    return(
+        <a href={url}>
+            {name}
+        </a>
+    )
+}
+
+export default Header;
