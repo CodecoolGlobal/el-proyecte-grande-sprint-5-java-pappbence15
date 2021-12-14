c268d3b4a3551309efcbc30ea2bdbed50312f5d9
csilla <erzsebet.lk@gmail.com>
2021-12-14 13:22:53 +0100

    feat: SingleProjectController now returns JSON for one project

--- src/main/java/com/codecool/progresstracker/controllers/view/SingleProjectController.java
+++ src/main/java/com/codecool/progresstracker/controllers/view/SingleProjectController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.UUID;
@@ -26,37 +27,31 @@ public class SingleProjectController {
         this.projectService = projectService;
     }
 
+    @ResponseBody
     @GetMapping("/admin/project/{projectId}")
-    public String adminProjectPage(Model model, @PathVariable UUID projectId) throws Exception {
+    public Project adminProjectPage(@PathVariable UUID projectId) throws Exception {
         Project project = projectService.getById(projectId);
 
         User user = userService.getLoggedInUser();
-
-        model.addAttribute("user", user);
         UserType userType = user.getUserType();
 
-        model.addAttribute("project", project);
-        model.addAttribute("progress", Math.round(project.getPercentage()*100) + " %");
-
         if (userType == UserType.ADMIN && project.getAdmins().contains(user)) {
-            return "admin_project_view";
+            return project;
         } else {
             throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
         }
     }
+
+    @ResponseBody
     @GetMapping("/owner/project/{projectId}")
-    public String ownerProjectPage(Model model, @PathVariable UUID projectId) throws Exception {
+    public Project ownerProjectPage(@PathVariable UUID projectId) throws Exception {
         Project project = projectService.getById(projectId);
 
         User user = userService.getLoggedInUser();
-
-        model.addAttribute("user", user);
         UserType userType = user.getUserType();
 
-        model.addAttribute("project", project);
-
         if (userType == UserType.PROJECT_OWNER && project.getOwner().equals(user)){
-            return "owner_project_view";
+            return project;
         } else {
             throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
         }
