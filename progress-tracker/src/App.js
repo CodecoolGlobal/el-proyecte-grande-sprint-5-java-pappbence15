2e85bec8fa9183254e1c0e6e0f7b5830e2ebcbc2
Papp Bence <papp.bence15@gmail.com>
2021-12-14 14:27:49 +0100

    feat: Change dynamic components

--- progress-tracker/src/App.js
+++ progress-tracker/src/App.js
@@ -5,14 +5,27 @@ import {useState} from "react";
 function App() {
     const [component, setComponent] = useState('All')
     const changeComponent = (newComponent) => setComponent(newComponent)
-    console.log(component)
   return (
+      <div>
     <div className="App">
       <header className="App-header">
       </header>
         <Navbar change={changeComponent}/>
     </div>
+          <div>
+              {renderDynamicComponent(component)}
+          </div>
+      </div>
   );
 }
-
+function renderDynamicComponent(component){
+    switch (component){
+        case 'All':
+            return(<p>test1</p>)
+        case 'Latest':
+            return (<p>test2</p>)
+        case 'Projects':
+            return (<p>test3</p>)
+    }
+}
 export default App;
