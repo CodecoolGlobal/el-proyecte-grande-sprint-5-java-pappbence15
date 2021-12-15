1de74e8a58451188905736d527f923279a35f99f
Papp Bence <papp.bence15@gmail.com>
2021-12-15 10:32:00 +0100

    merge: Merge changes

--- progress-tracker/src/App.js
+++ progress-tracker/src/App.js
@@ -1,10 +1,7 @@
 import './style/App.css';
-<<<<<<< HEAD
 import './style/Navbar.css';
-=======
 import './style/Header.css'
 import './style/Footer.css'
->>>>>>> 0e6176de4302c170e44fe98f865541bda43e6353
 import Navbar from "./Navbar";
 import Header from "./Header";
 import Footer from "./Footer";
@@ -16,13 +13,9 @@ function App() {
   return (
       <div>
     <div className="App">
-<<<<<<< HEAD
-        <Header/>
-=======
         <Header name={"Name of the Brand"}/>
         <Navbar change={changeComponent}/>
         <Footer owner={"©Hello World KFT"} creators={["csillalukacs", "Sjpeti97", "pappbence15", "JustBenS1"]} links={['https://github.com/csillalukacs', "https://github.com/Sjpeti97", "https://github.com/pappbence15", "https://github.com/JustBenS1"]}/>
->>>>>>> 0e6176de4302c170e44fe98f865541bda43e6353
     </div>
           <div className='dynamic-component-container'>
               <Navbar change={changeComponent}/>
