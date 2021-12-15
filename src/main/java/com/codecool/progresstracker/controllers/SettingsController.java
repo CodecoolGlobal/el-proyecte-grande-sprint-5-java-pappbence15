f2d11e1b5b2da01dcfbdc45343312542013d943c
Sjpeti97 <sjpeti97@gmail.com>
2021-12-15 14:49:57 +0100

    refactor: settings endpoints to work with new design

--- src/main/java/com/codecool/progresstracker/controllers/SettingsController.java
+++ src/main/java/com/codecool/progresstracker/controllers/SettingsController.java
@@ -2,13 +2,13 @@ package com.codecool.progresstracker.controllers;
 
 import com.codecool.progresstracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 @Controller
@@ -21,13 +21,20 @@ public class SettingsController {
         this.userService = userService;
     }
 
+    @ResponseBody
     @GetMapping("/settings")
-    public Map<String, Boolean> getSettings(@RequestBody UUID userId){
-        return userService.getUserSettings(userId);
+    public ResponseEntity<?> getSettings(){
+        return new ResponseEntity<>(userService.getLoggedInUser().getUserSettings(), HttpStatus.OK);
     }
 
-    @PostMapping("/settings/update")
-    public void setSettings(@RequestBody UUID userId, String key, boolean value){
-        userService.updateUserSettings(userId, key, value);
+    @ResponseBody
+    @PostMapping("/settings/update/{key}/{value}")
+    public void setSettings(@PathVariable String key, @PathVariable String value){
+        boolean newValue;
+        newValue = Objects.equals(value, "true");
+        System.out.println(key);
+        System.out.println(newValue);
+        userService.updateUserSettings(key, newValue);
+//        return new ResponseEntity<>(userService.getLoggedInUser().getUserSettings(), HttpStatus.OK);
     }
 }
