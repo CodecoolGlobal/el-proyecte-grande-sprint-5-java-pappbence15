3d5f80ad0f90a060655d7c1dd5e8e448f3984c59
JustBenS1 <bence1997asd@gmail.com>
2021-12-15 11:24:13 +0100

    refactor: import lombok annotations to clear up the code

--- src/main/java/com/codecool/progresstracker/model/LoginAttempt.java
+++ src/main/java/com/codecool/progresstracker/model/LoginAttempt.java
@@ -1,15 +1,11 @@
 package com.codecool.progresstracker.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 public class LoginAttempt {
     private final String username;
     private final String password;
-
-    public LoginAttempt(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 }
