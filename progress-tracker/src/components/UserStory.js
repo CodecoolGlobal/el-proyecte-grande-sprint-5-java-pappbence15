243756705c3dc3b07ec3fcef0fdfb368eebb5913
Papp Bence <papp.bence15@gmail.com>
2021-12-16 17:55:38 +0100

    feat: User stories on cards, normal test data

--- progress-tracker/src/components/UserStory.js
+++ progress-tracker/src/components/UserStory.js
@@ -1,10 +1,24 @@
 import {Star} from "./Star";
+import ProgressBar from "./ProgressBar"
+import {Card, CardContent, Typography} from "@mui/material";
 
 export default function UserStory(props){
     return(
-        <li>
+        <Card sx={{minWidth: 275}}>
+            <CardContent>
+                <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                    {props.story.name}
+                </Typography>
+                <ProgressBar size={"small"}
+                             percentage={100*(props.story.currentPercent)}/>
+                <Star full={props.story.favourite} id={props.story.id}/>
+            </CardContent>
+        </Card>
+        /*<li>
+            <ProgressBar size={"small"}
+                         percentage={100*(props.story.currentPercent)}/>
             {props.story.name}
             <Star full={props.story.favourite} id={props.story.id}/>
-        </li>
+        </li>*/
     )
 }
