b32da8c888a5d0d9ea0b35c0ebd042974d5b6acd
Papp Bence <papp.bence15@gmail.com>
2022-01-12 10:52:19 +0100

    refactor: ProjectController uses database

--- src/main/java/com/codecool/progresstracker/service/UserService.java
+++ src/main/java/com/codecool/progresstracker/service/UserService.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 @Service
 public class UserService {
     private User loggedInUser;
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserService(UserRepository userRepository) {
