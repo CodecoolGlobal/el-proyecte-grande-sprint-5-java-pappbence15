94561e5acfad67bbb7104758065a7a44dceeab15
Papp Bence <papp.bence15@gmail.com>
2022-01-13 13:54:14 +0100

    feat: Auto wire UserStoryService.

--- src/main/java/com/codecool/progresstracker/controllers/MockController.java
+++ src/main/java/com/codecool/progresstracker/controllers/MockController.java
@@ -1,8 +1,10 @@
 package com.codecool.progresstracker.controllers;
 
 import com.codecool.progresstracker.data_sample.CreateMockData;
+import com.codecool.progresstracker.model.UserStory;
 import com.codecool.progresstracker.service.ProjectService;
 import com.codecool.progresstracker.service.UserService;
+import com.codecool.progresstracker.service.UserStoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,18 +17,21 @@ public class MockController {
 
     private final UserService userService;
     private final ProjectService projectService;
+    private final UserStoryService userStoryService;
 
     @Autowired
     public MockController(UserService userService,
-                          ProjectService projectService) {
+                          ProjectService projectService,
+                          UserStoryService userStoryService) {
 
         this.projectService = projectService;
         this.userService = userService;
+        this.userStoryService = userStoryService;
     }
 
     @PostMapping("/init")
     public void init() throws ParseException {
-        CreateMockData createMockData = new CreateMockData(userService, projectService);
+        CreateMockData createMockData = new CreateMockData(userService, projectService, userStoryService);
         createMockData.spamMockData();
     }
 
