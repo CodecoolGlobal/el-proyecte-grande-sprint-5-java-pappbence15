89236b4818fc849d0638723fe8d52cfbc2c8e351
Papp Bence <papp.bence15@gmail.com>
2022-01-24 16:00:28 +0100

    feat: Add config, role classes.

---
+++ src/main/java/com/codecool/progresstracker/security/ApplicationSecurityConfig.java
@@ -0,0 +1,2 @@
+package com.codecool.progresstracker.security;public class ApplicationSecurityConfig {
+}
---
+++ src/main/java/com/codecool/progresstracker/security/ApplicationUserRole.java
@@ -0,0 +1,2 @@
+package com.codecool.progresstracker.security;public enum ApplicationUserRole {
+}
