96556fce1a0e398e96d9e179abb193efce840035
Papp Bence <papp.bence15@gmail.com>
2022-01-25 12:01:15 +0100

    feat: Add context to security config files.

--- src/main/java/com/codecool/progresstracker/security/ApplicationSecurityConfig.java
+++ src/main/java/com/codecool/progresstracker/security/ApplicationSecurityConfig.java
@@ -1,2 +1,33 @@
-package com.codecool.progresstracker.security;public class ApplicationSecurityConfig {
+package com.codecool.progresstracker.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+import static com.codecool.progresstracker.security.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/css/**", "/js/**", "logo.svg", "logo-for-progress-tracker.png", "/static/**").permitAll()
+                .antMatchers("/admin/**").hasRole(ADMIN.name())
+                .antMatchers("/owner/**").hasRole(OWNER.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout();
+
+    }
+
 }
