e4bc0c0859bc5eb8ba7d0282738e28496cd7bcac
Sjpeti97 <sjpeti97@gmail.com>
2022-01-27 10:13:27 +0100

    feat:now security config works with db users

--- src/main/java/com/codecool/progresstracker/security/ApplicationSecurityConfig.java
+++ src/main/java/com/codecool/progresstracker/security/ApplicationSecurityConfig.java
@@ -1,30 +1,28 @@
 package com.codecool.progresstracker.security;
 
 
+import com.codecool.progresstracker.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-
-import static com.codecool.progresstracker.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -32,34 +30,50 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**" , "/css/**", "/js/**", "logo.svg", "logo-for-progress-tracker.png", "/static/**").permitAll()
+                .antMatchers("/login", "/register", "/settings/**", "/css/**", "/js/**", "logo.svg", "logo-for-progress-tracker.png", "/static/**").permitAll()
 //                .antMatchers("/admin/**", "/owner/**", "/init").hasRole(ADMIN.name())
 //                .antMatchers("/owner/**").hasRole(OWNER.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin()
+                .defaultSuccessUrl("http://localhost:3000/", true)
                 .and()
-                .logout();
+                .logout()
+//                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login");
 
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails adminUser =  User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("Adminpassword"))
-                .roles(passwordEncoder.encode(ADMIN.name()))
-                .build();
-
-        UserDetails ownerUser =  User.builder()
-                .username("owner")
-                .password(passwordEncoder.encode("Ownerpassword"))
-                .roles(OWNER.name())
-                .build();
-
-        return new InMemoryUserDetailsManager(adminUser, ownerUser);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+    }
+
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails adminUser =  User.builder()
+//                .username("admin")
+//                .password(passwordEncoder.encode("Adminpassword"))
+//                .roles(passwordEncoder.encode(ADMIN.name()))
+//                .build();
+//
+//        UserDetails ownerUser =  User.builder()
+//                .username("owner")
+//                .password(passwordEncoder.encode("Ownerpassword"))
+//                .roles(OWNER.name())
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(adminUser, ownerUser);
+//    }
 
 }
