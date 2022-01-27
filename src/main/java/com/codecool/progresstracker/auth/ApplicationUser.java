1a1ab02a2ce48b0ebeddc64cd3f8558b37df14b5
Sjpeti97 <sjpeti97@gmail.com>
2022-01-27 10:14:19 +0100

    feat: add application user class to implement UserDetails

---
+++ src/main/java/com/codecool/progresstracker/auth/ApplicationUser.java
@@ -0,0 +1,75 @@
+package com.codecool.progresstracker.auth;
+
+import com.codecool.progresstracker.model.UserType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class ApplicationUser implements UserDetails {
+
+    private final UserType grantedAuthorities;
+    private final String password;
+    private final String userName;
+    private final boolean isAccountNonExpired = true;
+    private final boolean isAccountNonLocked = true;
+    private final boolean isCredentialsNonExpired = true;
+    private final boolean isEnabled = true;
+
+    public ApplicationUser(UserType grantedAuthorities,
+                           String password,
+                           String userName
+//                           boolean isAccountNonExpired,
+//                           boolean isAccountNonLocked,
+//                           boolean isCredentialsNonExpired,
+//                           boolean isEnabled
+    ) {
+        this.grantedAuthorities = grantedAuthorities;
+        this.password = password;
+        this.userName = userName;
+//        this.isAccountNonExpired = isAccountNonExpired;
+//        this.isAccountNonLocked = isAccountNonLocked;
+//        this.isCredentialsNonExpired = isCredentialsNonExpired;
+//        this.isEnabled = isEnabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(grantedAuthorities.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
