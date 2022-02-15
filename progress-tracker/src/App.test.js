af22a647e0c14b06437817f37be35cf41c28bcbd
Papp Bence <papp.bence15@gmail.com>
2022-02-15 11:52:54 +0100

    fix: App.test [build front-end].

--- progress-tracker/src/App.test.js
+++ progress-tracker/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
-test('renders learn react link', () => {   render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/GET IT DONE/i);
     expect(linkElement).toBeInTheDocument();
     });
